4925bb4f96ea43c4da3eb06eb89b1ac33a439130
devluish <devluishenrique@hotmail.com>
2025-11-11 15:59:10 -0300

    Commit

---
+++ src/revisao/LerNumero.kt
@@ -0,0 +1,12 @@
+// Ler um número e dizer se é par ou ímpar.
+
+package revisao
+
+class LerNumero {
+    fun main() {
+        println("Digite um número:")
+
+        val numero = readln().toInt()
+    }
+
+}
