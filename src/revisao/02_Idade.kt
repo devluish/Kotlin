6e5be2dc3ded943d0f2aff45956f0f789779ec37
devluish <devluishenrique@hotmail.com>
2025-11-12 14:22:23 -0300

    Exercicio 2

--- src/revisao/02_Idade.kt
+++ src/revisao/02_Idade.kt
@@ -1,7 +1,14 @@
 // Ler uma idade e dizer se é maior ou menor de idade.
-
+C:\Users\Luis\IdeaProjects\kotlinC:\Users\Luis\IdeaProjects\kotlin
 package revisao
 
 fun main() {
+    println("Digite sua idade")
+    val idade = readln().toInt()
 
+    if (idade >= 18){
+        println("A idade:$idade é maior")
+    } else{
+        println("A idade:$idade é menor")
+    }
 }
