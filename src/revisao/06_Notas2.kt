df929ee5e4f967f7ca1fe4e023a875b56114031d
devluish <devluishenrique@hotmail.com>
2025-11-12 17:26:20 -0300

    Finalizando exercicio 6

--- src/revisao/06_Notas2.kt
+++ src/revisao/06_Notas2.kt
@@ -4,8 +4,15 @@ package revisao
 
 fun main() {
     print("Insira sua nota: ")
-    var notaInserida = readln().toInt()
+    var notaInserida = readln().toDouble()
 
-    
-
+    val nota = when (notaInserida) {
+        in 9.0..10.0 -> "A"
+        in 7.0..8.9 -> "B"
+        in 5.0..6.9 -> "C"
+        in 3.0..4.9 -> "D"
+        in 0.0..2.9 -> "E"
+        else -> "Nota inválida"
+    }
+    println("Sua nota: $notaInserida é tem a nota: $nota")
 }
