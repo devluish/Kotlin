0a2499966ec75301ca1164e58cd31a64b66d32b8
devluish <devluishenrique@hotmail.com>
2025-11-13 15:22:56 -0300

    commit

--- src/revisao/08_Apelido.kt
+++ src/revisao/08_Apelido.kt
@@ -1,8 +1,14 @@
 // Ler um possível apelido (String?) e imprimir o tamanho usando ?. e ?: se for nulo/vazio.
 // Utilizando length
+// val nome: String? = null
+
+
 
 package revisao
 
 fun main() {
+    println("Digite seu apelido")
+    val apelido: String = readln()
+
 
 }
